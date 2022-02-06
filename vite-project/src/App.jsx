07982b15e7d0368e5f1bb7112c770f096e25a43a
gribdesbois <gribdesbois@gmail.com>
2022-02-06 21:57:48 +0100

    MessageList component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import NewMessageForm from './components/NewMessageForm'
+import MessageList from './components/MessageList'
 
 const App = () => {
-  const handleSend = (newMessage) => {}
+  const [messages, setMessages] = useState([])
+  const handleSend = (newMessage) => {
+    setMessages([newMessage, ...messages])
+  }
   return (
     <div>
       <NewMessageForm onSend={handleSend} />
+      <MessageList data={messages}> </MessageList>
     </div>
   )
 }
